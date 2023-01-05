fa071e6f8489c59c67d0b72f7d26390464f7f573
bomblito11 <maciekswiszcz11@gmail.com>
2023-01-05 22:04:10 +0100

    first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import simpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
+
+const gallery = document.querySelector('.gallery');
+
+galleryItems.forEach(image => {
+  gallery.insertAdjacentHTML(
+    'beforeend',
+    `<a class="gallery__link" href=${image.original} >
+      <img
+        class="gallery__image"
+        src=${image.preview}
+        data-source=${image.original}
+        alt=${image.description}
+      />
+    </a>`
+  );
+});
+
+const lightbox = new simpleLightbox('.gallery .gallery__link', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
