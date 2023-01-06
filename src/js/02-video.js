db732fe243ba79acf2c986fb703ae3e64a16a5e0
bomblito11 <maciekswiszcz11@gmail.com>
2023-01-06 20:11:33 +0100

    second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import Player from '@vimeo/player';
+import _ from 'lodash';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  _.throttle(e => {
+    localStorage.setItem('videoplayer-current-time', e.seconds);
+  }, 1000)
+);
+
+const getVideoCurrentTime = () => {
+  return localStorage.getItem('videoplayer-current-time');
+};
+
+player
+  .setCurrentTime(getVideoCurrentTime())
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
