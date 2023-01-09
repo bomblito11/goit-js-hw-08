93313d5ea762e3e2641d36cfdb75d2fd2129befd
bomblito11 <maciekswiszcz11@gmail.com>
2023-01-09 17:56:59 +0100

    third task add

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import _ from 'lodash';
 
+const formElement = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+const { email, message } = formElement.elements;
+
+formElement.addEventListener(
+  'input',
+  _.throttle(() => {
+    localStorage.setItem(
+      STORAGE_KEY,
+      JSON.stringify({
+        email: email.value,
+        message: message.value,
+      })
+    );
+  }, 500)
+);
+
+const retrievedInputs = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+window.addEventListener('load', () => {
+  if (retrievedInputs) {
+    email.value = retrievedInputs.email;
+    message.value = retrievedInputs.message;
+  }
+});
+
+formElement.addEventListener('submit', event => {
+  event.preventDefault();
+  localStorage.removeItem(STORAGE_KEY);
+  console.log({
+    email: formElement.email.value,
+    message: formElement.message.value,
+  });
+  formElement.reset();
+});
